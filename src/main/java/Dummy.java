7d8ae32f47408ff4be43857f770903e1b3dfb1c7
shoot-h <81802152+shoot-h@users.noreply.github.com>
2021-06-10 05:55:54 +0900

    Upload Complete Version

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Dummy {
     public void test() {
-        int a = 2;
+        int a;
+        a = 2;
         final int c = 1;
         if (a == 2) {
             System.out.println("1-1");
@@ -19,5 +20,18 @@ public class Dummy {
         map.put(a, "a");
         map.put(b, "b");
         System.out.println(map.get(c));
+        map.clear();
+        Map<obj, String> map1 = new HashMap<>();
+        obj obj1 = new obj (0,"n");
+        map1.put(obj1, "OK");
     }
+}
+
+class obj {
+	int obji = 0;
+	String objs = "a";
+	public obj(int obj0,String obj1) {
+		obji = obj0;
+		objs = obj1;
+	}
 }
--- src/main/java/visitor/VisiterForTutorial.java
+++ src/main/java/visitor/VisiterForTutorial.java
@@ -7,41 +7,60 @@ import java.lang.management.CompilationMXBean;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VisiterForTutorial extends ASTVisitor {
     CompilationUnit compilationUnit = null;
-    ArrayList<ExpressionStatement> nodestock = new ArrayList<>();
-    ArrayList<VariableDeclarationStatement> verstock = new ArrayList<>();
-    ArrayList<String> mapstock = new ArrayList<>();
-    ArrayList<String> namestock = new ArrayList<>();
+    ArrayList<String> mapnamestock = new ArrayList<>();//Save Veriable Name(Only Type:Map) 
+    ArrayList<String> vernamestock = new ArrayList<>();//Save Veriable Name(ALL)
+    Map<String,String> verTypestock = new HashMap<>();//Save Type for each Variable
+    int verjudge = 0;
+    String verType;
     public VisiterForTutorial(CompilationUnit compilationUnit){
         this.compilationUnit = compilationUnit;
     }
     
     public boolean visit(VariableDeclarationFragment node) {
-        namestock.add(node.getName().toString());
+    	if(verjudge == 2) {
+    		mapnamestock.add(node.getName().toString());
+    		//System.out.println(node.getName());
+    	}
+        if(verjudge != 0) {
+        	verTypestock.put(node.getName().toString(), verType);
+        	vernamestock.add(node.getName().toString());
+        }
+        
+        verjudge = 0;
         return false; // do not continue to avoid usage info
     }
     
     public boolean visit(VariableDeclarationStatement node) {
-    	verstock.add(node);
-    	if(node.getType().toString().contains("Map")) {
-    		System.out.println(node);
-    		return true;
+    	if(node.getType().toString().contains("Map<")) {
+    		//System.out.println(node.getType());
+    		verjudge = 2;
     	}
-    	else return false; // do not continue to avoid usage info
+    	else verjudge = 1;
+    	verType = node.getType().toString();
+    	return true; // do not continue to avoid usage info
     }
     
-    public boolean visit(ExpressionStatement node) {
-    	nodestock.add(node);
-    	Expression map1 = node.getExpression();
-    	for(int i = 0;i < namestock.size();i++) {
-        	if(map1.toString().contains(namestock.get(i))) {
-        		//System.out.println("Line "+compilationUnit.getLineNumber(node.getStartPosition()));
-        		System.out.println(map1);
-        		System.out.println(map1.structuralPropertiesForType());
+    	//((MethodInvocation)map1).arguments().get(0);
+
+    public boolean visit(MethodInvocation node) {
+    	for(int i = 0;i < mapnamestock.size();i++) {
+        	if(node.getExpression().toString().equals(mapnamestock.get(i))) {
+            	//System.out.println(node.arguments().get(0).toString());
+            	for(int j = 0;j < vernamestock.size();j++) {
+                	if(node.arguments().size() != 0 && node.arguments().get(0).toString().equals(vernamestock.get(j))) {
+                    	System.out.println(verTypestock.get(node.arguments().get(0).toString()));
+                	}
+            	}
         	}
-    	}  	
-        return true; // do not continue to avoid usage info
+    	}
+
+    	return true;
     }
+    
+    
 }
