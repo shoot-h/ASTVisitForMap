2eb4f9d548613168a7023c1ccc6e61e1e8dbe47c
shoot-h <81802152+shoot-h@users.noreply.github.com>
2021-10-01 01:25:18 +0900

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import org.eclipse.jdt.core.dom.AST;
 import java.io.File;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+
 import visitor.VisiterForTutorial;
 
 public class Main {
     public static void main(String[] args)  {
-        String pathTarget = args[0];
-        System.out.println("search " + new File(args[0]).getName());
+    	//String pathTarget = "/Users/shuhashimoto/eclipse-workspace/Map/src/Main.java";
+    	String pathTarget = "/Users/shuhashimoto/Downloads/kGenProg-master/src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java";
+        System.out.println("search " + new File(pathTarget).getName());
         String sourceTarget = util.FileUtil.readFile(pathTarget);
         ASTParser parser = ASTParser.newParser(AST.JLS3);
         parser.setSource(sourceTarget.toCharArray());
@@ -16,5 +18,7 @@ public class Main {
         final CompilationUnit compilationUnit = (CompilationUnit) parser.createAST(null);
 
         compilationUnit.accept(new VisiterForTutorial(compilationUnit));
+        
+        System.out.println("Finish");
     }
 }
